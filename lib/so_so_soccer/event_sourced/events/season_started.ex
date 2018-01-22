bd2ce068ca0bc8ed97541f4919a602e9b0c80c0b
Leif Gensert <leif@leif.io>
2018-01-22 20:54:15 +0700

    simplify season started event

--- lib/so_so_soccer/event_sourced/events/season_started.ex
+++ lib/so_so_soccer/event_sourced/events/season_started.ex
@@ -4,5 +4,5 @@ defmodule SoSoSoccer.EventSourced.Events.SeasonStarted do
           started_at: NaiveDateTime.t()
         }
 
-  defstruct [:id, :name, :country_id, :started_at]
+  defstruct [:id, ,:started_at]
 end
